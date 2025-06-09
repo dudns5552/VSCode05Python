0160827871e951104e9203fadd8016277633f39d
dudns5552 <dudns5552@gmail.com>
2025-06-09 18:00:36 +0900

    파일업데이트

--- 02operator3.py
+++ 02operator3.py
@@ -1,10 +1,6 @@
-#입력받은 숫자를 실수형으로 변환하여 변수에 저장 
 num1 = float(input("첫번째 숫자를 입력하세요:"))
 num2 = float(input("두번째 숫자를 입력하세요:"))
 
-#3, 4를 입력했다고 가정하면..
-
-#관계연산자
 result1 = num1 == num2
 print("같은지 비교", result1) #False
 
@@ -20,7 +16,6 @@ print("작은지 비교", result4) #True
 result5 = not (num1 > num2)
 print("관계식 판단의 부정", result5) #True 
 
-#논리연산자
 logical1 = result1 and result2 #F and T ==> False
 logical2 = result1 or result2 #F or T ==> True
 logical3 = not (result1 or result2) #not(True) ==> False
@@ -28,3 +23,4 @@ logical3 = not (result1 or result2) #not(True) ==> False
 print('logical1', logical1)
 print('logical2', logical2)
 print('logical3', logical3)
+
--- mod1.py
+++ mod1.py
@@ -1,6 +1,6 @@
-# 덧셈, 뺄셈을 위한 함수 정의
+#덧셈, 뺄셈을 위한 함수 정의 
 def add(a, b):
-  return a + b
+    return a + b
 
-def sub(a, b):
-  return a - b
+def sub(a, b): 
+    return a-b
